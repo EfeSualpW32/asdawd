99a846208fc20c079f749833e2a9c6ca905be364
Glitch (purple-gold-pie) <none>
2021-09-22 11:59:52 +0000

    🙆🐯 Checkpoint ./commands/ekonomi/günlük.js:25470421/1992 ./commands/ekonomi/hesap-bilgi.js:25470421/1718 ./commands/ekonomi/market.js:25470421/3586 ./commands/ekonomi/parayatır.js:25470421/2359 ./commands/ekonomi/para.js:25470421/23 ./commands/ekonomi/para-gönder.js:25470421/2546 ./commands/ekonomi/ekonomi-yardım.js:25470421/1984

--- commands/ekonomi/ekonomi-yardım.js
+++ commands/ekonomi/ekonomi-yardım.js
@@ -1,16 +1,28 @@
-const discord = require('discord.js')
-exports.run = async(client, message , args) => {
-let engin = new discord.MessageEmbed()
-.setDescription(":moneybag: **PARA KAZANMA KOMUTLARI** :moneybag: \n\n  `ara`  \n \n `çalış` \n  \n`dilen` \n \n :credit_card: **BANKA KOMUTLARI** :credit_card: \n\n  `para-yatır`  \n  \n `para-çek` \n \n `banka` \n  \n `çanta` \n \n :toolbox: **SATIN ALIM VE MARKET KOMUTLARI** :toolbox: \n\n  `market`  \n  \n `satın-al` \n  \n `sat`  \n  \n `kullan` \n \n :clock12: **SÜRELİ KOMUTLAR** :clock12: \n\n `çal` \n \n `günlük`")
-message.channel.send(engin)
-}
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0
-  };
+const Discord = require('discord.js')
+
+exports.run = async(client, message) => {
   
-  exports.help = {
-    name: 'e-yardım' 
-  }
+    const y = new Discord.MessageEmbed()
+    .setColor('RED')
+    .addField(':small_red_triangle_down:','`!günlük-para : Günlük Para (24 Saatte Bir Kullanılabilir)`')
+    .addField(':small_red_triangle_down:','`!hesap-aç : Banka Hesap Açar (+20 Lira Verir) `')
+    .addField(':small_red_triangle_down:','`!hesabımı-sil : Banka Hesabınızı Siler`')
+    .addField(':small_red_triangle_down:','`!hesap : Banka Hesabınız Hakkında Bilgi Verir`')
+    .addField(':small_red_triangle_down:','`!para-gönder : Belirttiğiniz Kişiye Belirttiğiniz Miktarda Para Gönderir`')
+    .addField(':small_red_triangle_down:','`!para : Bankadaki Paranızı Belirtir`')
+    .addField(':small_red_triangle_down:','`!market : Markteti Gösterir`')
+    .addField(':small_red_triangle_down:','`!istatistik : Botunuzun İstatistiklerini Gösterir`')
+    .setFooter(`${client.user.username}`, client.user.avatarURL)
+    .setThumbnail('https://cdn.discordapp.com/attachments/727202608133046415/727205052808298546/Kobs_server.png') 
+    return message.channel.send(y)
+};
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+  aliases: [],
+ permLevel: 0
+};
+
+exports.help = {
+ name: 'e-yardım'
+};
--- commands/ekonomi/market.js
+++ commands/ekonomi/market.js
@@ -1,18 +1,59 @@
-const discord = require('discord.js')
-const db = require('quick.db')
-exports.run = async(client, message , args) => {
-let engin = new discord.MessageEmbed()
-.setTitle('Market menüsü')
-.setDescription(`:euro: **PARANA PARA KATMAK İÇİN EŞYA** :euro:\n\`Eşya Satın Almak İçin ↓\`\n\`?satın-al (eşya-adı)\` \n \n \`balta\` \n \n \`olta\` \n \n \`silah\` \n \n :key: **KORUNMA EŞYALARI** :key: \n \n \`padlock\` \n \n :gift: **SANŞ EŞYALARI** :gift: \n \n \`bitcoin\``)
-message.channel.send(engin)
+const Discord = require('discord.js')
+const db = require('quick.db');
+
+
+exports.run = async (client, message, args) => {
+  
+  if (!args[0]) return message.reply(`Mevcut Markettekiler : \n\n\`\`\`\n Bıçak : 500 TL\n Silah : 1000 TL\n Haydar : 150 TL`)
+  
+  if (args[0] === 'Silah') {
+    let para = await db.get(`para_${message.author.id}`) 
+    let fiyatcık = 1000 // istediğiniz fiyat
+    
+    
+    if (para < fiyatcık) return message.reply('Yeterli Paran Yok!')
+    
+    db.set(`ìştebişiler_${message.author.id}`, "aktifdir")
+    db.add(`para_${message.author.id}`, -fiyatcık)
+    
+    return message.reply(`Ürünü Başarıyla Aldınız`)
+  }
+    if (!args[0]) return message.reply(`Mevcut Markettekiler : `)
+  
+  if (args[0] === 'Bıçak') {
+    let para = await db.get(`para_${message.author.id}`) 
+    let fiyatcık = 500 // istediğiniz fiyat
+    
+    
+    if (para < fiyatcık) return message.reply('Yeterli Paran Yok!')
+    
+    db.set(`ìştebişiler_${message.author.id}`, "aktifdir")
+    db.add(`para_${message.author.id}`, -fiyatcık)
+    
+    return message.reply(`Ürünü Başarıyla Aldınız`)
+  }
+    if (args[0] === 'haydar') {
+    let para = await db.get(`para_${message.author.id}`) 
+    let fiyatcık = 150 // istediğiniz fiyat
+    
+    
+    if (para < fiyatcık) return message.reply('Yeterli Paran Yok!')
+    
+    db.set(`ìştebişiler_${message.author.id}`, "aktifdir")
+    db.add(`para_${message.author.id}`, -fiyatcık)
+    
+    return message.reply(`Ürünü Başarıyla Aldınız`)
+  }
+
+
+  
 }
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
+  
     aliases: [],
     permLevel: 0
-  };
-  
-  exports.help = {
-    name: 'market' 
-  }
+}
+
+exports.help = {
+    name: 'market'
+}
