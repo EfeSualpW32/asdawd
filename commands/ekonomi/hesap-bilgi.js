3ff5fb741cee95ca43ccf4f967609bacbd29fb0d
Glitch (purple-gold-pie) <none>
2021-09-22 13:09:06 +0000

    🎸😂 Checkpoint ./commands/ekonomi/hesap-bilgi.js:25470421/308 ./commands/ekonomi/market.js:25470421/81

--- commands/ekonomi/hesap-bilgi.js
+++ commands/ekonomi/hesap-bilgi.js
@@ -5,13 +5,15 @@ const ms = require('ms')
 exports.run = async (client, message, args) => {
   
 let kişi = message.mentions.users.first() || message.author
-let envanter = await db.fetch(`envanter_${message.author.id}`) || 'Eşyanız yok'
+let bıçak = await db.fetch(`bıçak_${message.author.id}`) || '0'
+let silah = await db.fetch(`silah_${message.author.id}`) || '0'
+let haydar = await db.fetch(`haydar_${message.author.id}`) || '0'
   let parapara = await db.fetch(`para_${kişi.id}`) || 0  
   /// ARKADASLAR CALMAYIN BIR CODER DERKI ÇALAN KODIR DEĞİL 
     let isim = await db.fetch(`hesapisim_${kişi.id}`) || 'Hesap Yok'
 
  const sa = new Discord.MessageEmbed()
- .setDescription(`Kullanıcı: ${kişi} \n Parası : ${parapara} \n Hesap Adı ${isim} \n Envanter : ${envanter}`)
+ .setDescription(`Kullanıcı: ${kişi} \n Parası : ${parapara} \n Hesap Adı ${isim} \n Envanter :\n Bıçak :${bıçak}\n Silah : ${silah}\n Haydar : ${haydar}`)
  return message.channel.send(sa)
 };
 ///HAZIRLAYANLAR Clearly_ , FORCEX.js
--- commands/ekonomi/market.js
+++ commands/ekonomi/market.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
   if (!args[0]) return message.reply(`Mevcut Markettekiler : \n\n\`\`\`\n bıçak : 500 TL\n silah : 1000 TL\n haydar : 150 TL\n\`\`\``)
   
   if (args[0] === 'silah') {
-     db.push(`envanter_${message.author.id}`,'silah')
+     db.push(`silah_${message.author.id}`, +1)
     let para = await db.get(`para_${message.author.id}`) 
     let fiyatcık = 1000 // istediğiniz fiyat
     
@@ -22,7 +22,7 @@ exports.run = async (client, message, args) => {
     if (!args[0]) return message.reply(`Mevcut Markettekiler : `)
   
   if (args[0] === 'bıçak') {
-   db.push(`envanter_${message.author.id}`,'bıçak')
+   db.push(`bıçak_${message.author.id}`, +1)
     let para = await db.get(`para_${message.author.id}`) 
     let fiyatcık = 500 // istediğiniz fiyat
     
@@ -35,7 +35,7 @@ exports.run = async (client, message, args) => {
     return message.reply(`Ürünü Başarıyla Aldınız`)
   }
     if (args[0] === 'haydar') {
-    db.push(`envanter_${message.author.id}`,'haydar')
+    db.push(`haydar_${message.author.id}`, +1)
     let para = await db.get(`para_${message.author.id}`) 
     let fiyatcık = 150 // istediğiniz fiyat
     
@@ -43,7 +43,7 @@ exports.run = async (client, message, args) => {
     if (para < fiyatcık) return message.reply('Yeterli Paran Yok!')
     
     db.set(`ìştebişiler_${message.author.id}`, "aktifdir")
-     db.push(`para_${message.author.id}`, -fiyatcık)
+    db.add(`para_${message.author.id}`, -fiyatcık)
     
     return message.reply(`Ürünü Başarıyla Aldınız`)
   }
