b6062a30282f66b5d9747d12e5c0056bb6220412
Glitch (purple-gold-pie) <none>
2021-09-27 13:48:44 +0000

    🎤💍 Checkpoint ./commands/extras/inial.js:25470421/350 ./commands/ekonomi/deneme.js:25470421/458

--- commands/ekonomi/deneme.js
+++ commands/ekonomi/deneme.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
+const canvas = require('canvas');
+
 var ayarlar = require("../../config");
 const Canvas = require('canvas')
-    , Image = Canvas.Image
     , Font = Canvas.Font
     , path = require('path');
 const request = require('node-superfetch');
@@ -12,11 +13,11 @@ exports.run = async (client, message, args) => {
   
 let parapara = await db.fetch(`para_${kişi.id}`) || 0 
 
-            const { createCanvas, loadImage } = require('canvas');
-            const canvas = createCanvas(1092, 678);
+
+            const canvas = Canvas.createCanvas(1092, 678);
             const ctx = canvas.getContext("2d");
 
-            const background = await Canvas.loadImage('/app/assets/oooo.png');
+            const background = await Canvas.loadImage('https://cdn.glitch.com/0bb90534-d183-4b5d-a865-453271102d9c%2Foooo.png?v=1632749446473');
             ctx.drawImage(background , 0 ,0 , canvas.width , canvas.height);
             
             const avatar = await Canvas.loadImage(message.author.displayAvatarURL())
@@ -32,7 +33,7 @@ let parapara = await db.fetch(`para_${kişi.id}`) || 0
             
             const attachment = new Discord.MessageAttachment(
               canvas.toBuffer(),
-              "ooo.png"
+              "https://cdn.glitch.com/0bb90534-d183-4b5d-a865-453271102d9c%2Foooo.png?v=1632749446473"
             );
             
             message.channel.send(attachment)
