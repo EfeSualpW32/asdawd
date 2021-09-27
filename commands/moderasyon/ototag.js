ed705d0d852e1100f2bcbf745650e2d40b3b532e
Glitch (purple-gold-pie) <none>
2021-09-27 06:44:41 +0000

    🐚🌍 Checkpoint ./commands/moderasyon/ototag.js:25470421/1034

--- commands/moderasyon/ototag.js
+++ commands/moderasyon/ototag.js
@@ -10,16 +10,21 @@ exports.run = async(client, message, args) => {
     var prefix = "?";
   }
   };
-   if(!args[0]) return message.reply (`Bir seçenek belirtiniz. Eğer kullanımı bilmiyorsanız \`${prefix}ototag ayarla yardım\``)
+   if(!args[0]) return message.reply (`Bir seçenek belirtiniz. Eğer kullanımı bilmiyorsanız \`${prefix}ototag yardım\``)
   if (!message.member.permissions.has("ADMINISTRATOR")) return;
   if(!args[0] || args[0] !== "sıfırla"){
-  var tag = args.slice(0).join(' ')
+  var tag = args[2]
   if(!tag) return message.channel.send("Bir tag belirt!")
       if(args[0] == "ayarla" || args[0] == "aç") {
+           if(args[1] == "kanal" ||args[1] == "channel") {
+        var ales = message.mentions.channels.first() || message.guild.channel.cache.get(args[2]);
+    if(!ales) return message.reply('Bir kanal belirtiniz.')
+    qdb.set(`ototagkanali_${message.guild.id}`, ales.id)
+    return message.reply(new Discord.MessageEmbed().setColor(renks).setDescription('Ototag kanalı başarı ile ayarlandı.'))}
     qdb.set(`ototag_${message.guild.id}`, tag)
           qdb.set(`ototags_${message.guild.id}`,`<:onn:891927243574628382>`)
     return message.channel.send("Oto Tag Ayarlandı!")
-  }
+  }}
   if(args[0] == "sıfırla"){
          qdb.delete(`ototags_${message.guild.id}`)
     qdb.delete(`ototag_${message.guild.id}`)
@@ -27,18 +32,13 @@ exports.run = async(client, message, args) => {
     return message.channel.send("Oto Tag Sıfırlandı!")
     
   }
-   if(args[0] == "kanal" ||args[1] == "channel") {
-        var ales = message.mentions.channels.first() || message.guild.channel.cache.get(args[2]);
-    if(!ales) return message.reply('Bir kanal belirtiniz.')
-    qdb.set(`ototagkanali_${message.guild.id}`, ales.id)
-    return message.reply(new Discord.MessageEmbed().setColor(renks).setDescription('Ototag kanalı başarı ile ayarlandı.'))}
   
-   if(args[2] == "yardım" || args[0] == "help") {
+   if(args[0] == "yardım" || args[0] == "help") {
         const embedv1 = new Discord.MessageEmbed()
         .setColor(renks)
         .setTitle('Oto-Tag Yardım')
-        .addField(`${prefix}ototag kanal`,'Ototag kanalını ayarlar')
-        .addField(`${prefix}ototag rol`,"Ototag rolünü ayarlar")
+        .addField(`${prefix}ototag ayarla kanal`,'Ototag kanalını ayarlar')
+        .addField(`${prefix}ototag ayarla (tag)`,"Ototag  ayarlar")
         .addField(`${prefix}ototag sıfırla`,"Ototag veritabanını sıfırlar")
         .setFooter("EFDA OTOROL")}
 }
