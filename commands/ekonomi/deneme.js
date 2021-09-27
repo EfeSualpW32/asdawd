f0215864207b293dfef8207f371c435088c00cca
Glitch (purple-gold-pie) <none>
2021-09-27 12:51:09 +0000

    🍜🕍 Checkpoint ./commands/ekonomi/deneme.js:25470421/919

--- commands/ekonomi/deneme.js
+++ commands/ekonomi/deneme.js
@@ -13,13 +13,11 @@ exports.run = async (client, message, args) => {
 let parapara = await db.fetch(`para_${kişi.id}`) || 0 
 
             const { createCanvas, loadImage } = require('canvas');
-   var image = await loadImage("png.png");
+            
             const canvas = createCanvas(1092, 678);
             const ctx = canvas.getContext("2d");
 
-            const background = await Canvas.loadImage(
-              "https://cdn.discordapp.com/attachments/611466015582322700/668155571492356117/woxecredit.png"
-            );
+            const background = await Canvas.loadImage('https://cdn.glitch.com/0bb90534-d183-4b5d-a865-453271102d9c%2Fwoxecredit.png?v=1632743568846');
             ctx.drawImage(background , 0 ,0 , canvas.width , canvas.height);
             
             const avatar = await Canvas.loadImage(message.author.displayAvatarURL())
